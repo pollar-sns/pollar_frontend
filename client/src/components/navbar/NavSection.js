b7ef5c06f0ffc3a88ad5b574e3c5ad097223f06b
eazae <xynezae@gmail.com>
2022-02-03 21:36:34 +0900

    add interest selection in signup

--- client/src/components/navbar/NavSection.js
+++ client/src/components/navbar/NavSection.js
@@ -159,7 +159,7 @@ export default function NavSection({ navConfig, ...other }) {
 
   return (
     <Box {...other}>
-      <List disablePadding>
+      <List disablePadding sx={{ display: 'flex', flexdirection: 'row' }}>
         {navConfig.map((item) => (
           <NavItem key={item.title} item={item} active={match} />
         ))}
--- client/src/components/signup/SelectInterests.js
+++ client/src/components/signup/SelectInterests.js
@@ -1,40 +1,108 @@
 import { useEffect, useState } from 'react';
 import { getAllCategories } from '../../services/api/CategoryApi';
-import Chip from '@mui/material/Chip';
-import { styled } from '@mui/system';
+import { Chip, Stack, Typography } from '@mui/material';
 
-const ListItem = styled('li')(({ theme }) => ({
-  margin: theme.spacing(0.5),
-}));
-
-export default function SelectInterests() {
+export default function SelectInterests({ signupInfo }) {
+  // 전체 카테고리
   const [categoryList, setCategoryList] = useState([]);
+  // 사용자 선택 관심분야
+  const [interestList, setInterestList] = useState([]);
 
   const getList = async () => {
     const list = await getAllCategories();
-    console.log(list);
-    setCategoryList(list);
+    // setCategoryList(list);
+    // 대분류대로 배열에 삽입
+    let bigCategoryList = {};
+    list.forEach((item) =>
+      typeof bigCategoryList[item.categoryNameBig] === 'undefined'
+        ? (bigCategoryList[item.categoryNameBig] = [item])
+        : bigCategoryList[item.categoryNameBig].push(item)
+    );
+    setCategoryList(bigCategoryList);
   };
+
+  function CategoryGroup({ bigCategoryGroup }) {
+    return (
+      <>
+        <Typography variant="body2" align="left" sx={{ color: 'text.secondary', mt: 1 }}>
+          {bigCategoryGroup[0]}
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          {bigCategoryGroup[1].map((item) => (
+            <Chip
+              key={item.categoryId}
+              label={item.categoryNameSmall}
+              onClick={() => selectCategory(item)}
+              // clickable={interestList.includes(item)}
+            />
+          ))}
+        </Stack>
+      </>
+    );
+  }
+
+  /* 관심분야 선택 해제 */
+  const handleDelete = (unselected) => {
+    const filteredList = interestList.filter(
+      (item) => item.categoryNameSmall !== unselected.categoryNameSmall
+    );
+    setInterestList(filteredList);
+    // 삭제한 카테고리는 다시 전체 목록에 반영
+    console.log(filteredList);
+  };
+
+  /* 관심분야 선택 추가 */
+  const selectCategory = (selected) => {
+    if (interestList.length >= 3) {
+      // todo
+      alert('최대 3개 선택가능');
+    } else {
+      // 중복검사
+      console.log(interestList);
+      let existing = false;
+      for (let i of interestList) {
+        if (selected.categoryId === i.categoryId) {
+          existing = true;
+          alert('중복');
+          break;
+        }
+      }
+      if (!existing) setInterestList((currentArray) => [selected, ...currentArray]);
+    }
+  };
+
+  /* 관심분야 선택완료 시 */
+  // todo
+
   useEffect(() => {
+    // 관심분야 목록 API 호출
     getList();
   }, []);
 
-  // todo
-  const selectCategory = () => {
-    console.log('선택');
-  };
   return (
     <>
-      {/* <ListItem key={data.key}>
-        <Chip
-          icon={icon}
-          label={data.label}
-          onDelete={data.label === 'React' ? undefined : handleDelete(data)}
-        />
-      </ListItem> */}
-      {categoryList.map((category) => {
-        <Chip label={category} onClick={selectCategory} />;
-      })}
+      <Typography variant="body1" align="left" sx={{ color: 'text.secondary', mt: 3, mb: 1 }}>
+        선택한 관심분야
+      </Typography>
+      <Stack direction="row" spacing={1}>
+        {interestList.map((item, index) => (
+          <Chip
+            key={item.categoryId}
+            label={item.categoryNameSmall}
+            onDelete={() => handleDelete(item)}
+            color="success"
+          />
+        ))}
+      </Stack>
+      <Typography variant="body1" align="left" sx={{ color: 'text.secondary', mt: 3, mb: 1 }}>
+        카테고리 전체목록
+      </Typography>
+
+      <Stack spacing={2}>
+        {Object.entries(categoryList).map((bigCategoryGroup, index) => (
+          <CategoryGroup key={index} bigCategoryGroup={bigCategoryGroup} />
+        ))}
+      </Stack>
     </>
   );
 }
